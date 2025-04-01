f8168368157ad7a53c1a6270e17503e96ae013c5
LNapolitano <lucasnap1@gmail.com>
2025-04-01 11:17:13 -0700

    Updated to use proper argparsing and some code consolidated

--- MgII_NMF_HP.py
+++ MgII_NMF_HP.py
@@ -1,32 +1,31 @@
 #Constants------------------------------------
-first_line_wave = 2796.3543
-second_line_wave = 2803.5315
+first_line_wave = 2796.3543 #first line of MgII Doublet
+second_line_wave = 2803.5315 #second line of MgII Doublet
 rf_line_sep = second_line_wave-first_line_wave
 
 #Doublet Finder Hyperparameters
-rf_err_margain = 1.0
-kernel_smooth = 2
-med_filt_size_tight = 19
-med_filt_size_wide = 39
+rf_err_margain = 1.0 #acceptable difference from expected line seperation
+kernel_smooth = 2 #sttdev value of gaussian smoothing kernel used in calculating residual for doublet finding step
+med_filt_size_tight = 19 #narrow median filter used in constructing doublet finder continuum
+med_filt_size_wide = 39 #wider median filter used in constructing doublet finder continuum
 
-med_filt_size_fit = 85
+med_filt_size_fit = 85 #very wide median filter used as a fallback continuum when NMF fit is poor
 
-snr_threshold_low = 1.5
-snr_threshold_high = 2.5
+snr_threshold_low = 1.5 #threshold applied to second line for doublet fit candiates
+snr_threshold_high = 2.5 #threshold applied to first line for doublet fit candiates
 
 #MCMC Hyperparamets
-sub_region_size = 40
-min_accept_frac = 0.45
-min_tol = 100
-nwalkers = 32
-ndim = 5 #not really a hyper
-chain_length = 15000
-chain_discard = 3000
+sub_region_size = 40 #number of pixels around line used in determining region for MCMC fit
+min_accept_frac = 0.45 #minimum acceptance fraction necessary for MCMC process
+min_tol = 100 #minimum chain length / max(autocorrelation time estimate) necessary for MCMC process
+nwalkers = 32 #number of probability space walkers used in MCMC
+ndim = 5 #number of parameters in MCMC fit (redshift, line amplitudes, line widths)
+chain_length = 15000 #total MCMC step duration
+chain_discard = 3000 #MCMC steps discarded from beginning of process before determining values and storing chain
 #----------------------------------------------------
 
 #multiprocessing seteup
 import multiprocessing
-nprocesses = multiprocessing.cpu_count() // 8
       
 #standard imports
 import numpy as np
@@ -35,11 +34,10 @@ import h5py
 import sys
 import desispec.io
 import fitsio
-
 from astropy import modeling
-#from astropy.table import Table
+from argparse import ArgumentParser
 
-import emcee
+import emcee #this will require some python path setup and being cloned from git
 
 from desimodel.footprint import radec2pix
 from desispec.coaddition import coadd_cameras
@@ -47,22 +45,20 @@ from desispec.coaddition import coadd_cameras
 #doublet finder setup
 from astropy.convolution import Gaussian1DKernel
 from astropy.convolution import convolve
-
 from scipy.signal import medfilt
 kernel = Gaussian1DKernel(stddev = kernel_smooth)
 
+#used in selecting groups of consecutive negative values in doublet finder
 from operator import itemgetter
 from itertools import *
 fitter = modeling.fitting.LevMarLSQFitter()
 model = modeling.models.Gaussian1D()
 
-#using the main and sv cmx
-from desitarget.cmx.cmx_targetmask import cmx_mask
-from desitarget.targets import main_cmx_or_sv
-
+#used in determining QSO continuum
 import NMF_quasar_SEDs_decomposition_module as NMF
 NMF_basis = NMF.load_quasar_NMF_basis()
 
+#MgII line model
 def MgII_Model(theta,x):
     z,a1,a2,s1,s2 = theta
     #determine peak centers
@@ -73,7 +69,7 @@ def MgII_Model(theta,x):
     model = a1*np.exp((-(x-m1)**2)/(2*s1**2))+a2*np.exp((-(x-m2)**2)/(2*s2**2))
     return model
 
-#basic chi2 function
+#basic chi2 function, used in determining continuum goodness of fit
 def chi2(obs,cal,var,reduced = True):
     c2 = np.sum((obs-cal)**2 / var)
     
@@ -82,11 +78,12 @@ def chi2(obs,cal,var,reduced = True):
     else:
         return c2
 
+#These next three functions used in MCMC process
 #likelihood fnc
 def log_likelihood(theta, x, y, yerr):
-    #generative model
+    #generate model
     model = MgII_Model(theta,x)
-    #error into variance
+    #turn error into variance
     sigma2 = yerr ** 2
     #Actual Likelihood fnc
     return -0.5 * np.sum((y - model) ** 2 / sigma2 + np.log(sigma2))
@@ -94,7 +91,7 @@ def log_likelihood(theta, x, y, yerr):
 #prior fnc, could contain more info on reasonable redshifts, heights and widths
 def log_prior(theta,z_low,z_high):
     z,a1,a2,s1,s2 = theta
-    #if -100 < a1 < 100 and  -100 < a2 < 100 and 0 < s1  and  0 < s2  and z_low < z < z_high:
+    #standard deviations always positive and redshift in acceptable range
     if 0 < s1 and 0 < s2 and z_low < z < z_high:
         return 0.0
     return -np.inf
@@ -106,6 +103,7 @@ def log_probability(theta, x, y, yerr, z_low, z_high):
         return -np.inf
     return lp + log_likelihood(theta, x, y, yerr)
 
+#this function applies the MCMC process given an output from the doublet finder
 def Doublet_MCMC(Doublet,x_spc,y_flx,y_err):
     #spectrum info
     x_spc,y_flx,y_err = x_spc,y_flx,y_err
@@ -141,14 +139,10 @@ def Doublet_MCMC(Doublet,x_spc,y_flx,y_err):
 
     #could widen this inital guess range, don't think it matters though
     p0 = initial + 1e-4 * np.random.randn(nwalkers, ndim)
-    
-    #print(z,z_low,z_high)
-    #print(reg_wave,reg_flx,reg_err,z_low,z_high)
 
     #setup sampler with args
     sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args = [reg_wave,reg_flx,reg_err,z_low,z_high])
 
-    #print(p0)
     #burn-in
     state = sampler.run_mcmc(p0, 100)
     sampler.reset()
@@ -164,9 +158,6 @@ def Doublet_MCMC(Doublet,x_spc,y_flx,y_err):
     tau = sampler.get_autocorr_time(discard = chain_discard,quiet = True)
     implied_tol = chain_length/max(tau)
     mean_accept_frac = np.mean(sampler.acceptance_fraction)
-    
-    #print('MCMC fit TOL = {}, MAF = {}'.format(implied_tol,mean_accept_frac))
-    
     
     #mcmc quality cuts, if passed return the samples
     if(implied_tol > min_tol and mean_accept_frac > min_accept_frac):
@@ -240,8 +231,8 @@ def doublet_finder(continuum,x_spc,y_flx,y_err,min_z):
     return np.array(doublets)
 
 def Doublet_Detection(cat_subset,hp):
+    #for the healpixel determine the approriate prefix pixel (i.e. the directory structure)
     hp_str = str(hp)
-    
     if int(hp) < 100:
         hp_short = '0'
     else:
@@ -251,7 +242,7 @@ def Doublet_Detection(cat_subset,hp):
 
     #format output directory and filename
     out_dir = '{}/{}/{}'.format(out_base_dir,hp_short,hp_str)
-    print('Output Directory:',out_dir)
+    print('Output Directory:', out_dir)
     out_fn = 'MgII-Abs-Chains-{}.hdf5'.format(hp_str)
     #names of model params for h5py formatting
     names = ['z','Amp1','Amp2','StdDev1','StdDev2']
@@ -316,8 +307,6 @@ def Doublet_Detection(cat_subset,hp):
                 doublet_cont = c_tight*(1-np.arange(f_scale )/f_scale) + c_wide*(np.arange(f_scale )/f_scale)
                 #run doublet finder
                 doublets = doublet_finder(doublet_cont,x_spc,smooth_yflx,y_err,min_z)
-                
-                #print(doublets)
 
                 #NEW code for NMF/medfilt continuum conditions
                 NMF_fail = False
@@ -337,7 +326,7 @@ def Doublet_Detection(cat_subset,hp):
                     fitting_cont = medfilt_cont
                     Cont_method = 'Medfilt'
                 
-                #if NMF is poorly fit
+                #if NMF is poorly fit determine fitting continuum based on chi2 values
                 elif out[-1] > 4.0:
                     
                     #calculate new medilt and chi2
@@ -356,7 +345,7 @@ def Doublet_Detection(cat_subset,hp):
                     fitting_cont = out[-2]
                     Cont_method = 'NMF'
 
-
+                #run each doublet candidate through MCMC process
                 for doublet in doublets:
                     chain = Doublet_MCMC(doublet,x_spc,y_flx-fitting_cont,y_err)
 
@@ -376,137 +365,120 @@ def Doublet_Detection(cat_subset,hp):
                         for k in range(ndim):
                             grp.create_dataset(names[k],dtype = float,data = chain[:,k])
 
-#These pased as command line args
-redux = str(sys.argv[1])
-reduction_base_dir = '/global/cfs/cdirs/desi/spectro/redux/{}/'.format(redux)
-
-#open catalog
-if redux == 'fuji' or redux == 'guadalupe':
-    out_base_dir = '/pscratch/sd/l/lucasnap/MgII_Abs_Chains/{}-NMF'.format(redux)
-    
-    QSOcat_fp = '/global/cfs/cdirs/desi/users/edmondc/QSO_catalog/{}/QSO_cat_{}_healpix_only_qso_targets.fits'.format(redux,redux)
-    QSOcat = fitsio.read(QSOcat_fp,'QSO_CAT')
-    #restrict to only dark time exposures
-    QSOcat = QSOcat[QSOcat['PROGRAM'] == 'dark']
-    
-elif redux == 'iron':
-    out_base_dir = '/pscratch/sd/l/lucasnap/MgII_Abs_Chains/{}-NMF'.format(redux)
-    
-    QSOcat_fp = '/global/cfs/cdirs/desi/survey/catalogs/Y1/QSO/{}/QSO_cat_{}_main_dark_healpix_only_qso_targets_vtest.fits'.format(redux,redux)
-    QSOcat = fitsio.read(QSOcat_fp,'QSO_CAT')
-    #restrict to only dark time exposures
-    QSOcat = QSOcat[QSOcat['PROGRAM'] == 'dark']
-
-elif redux == 'fuji_all':
-    redux = 'fuji'
-    reduction_base_dir = '/global/cfs/cdirs/desi/spectro/redux/{}/'.format(redux)
-    out_base_dir = '/pscratch/sd/l/lucasnap/MgII_Abs_Chains/{}-ALL'.format(redux)
-    
-    QSOcat_all_fp = '/global/cfs/cdirs/desi/users/edmondc/QSO_catalog/{}/QSO_cat_{}_healpix.fits'.format(redux,redux)
-    QSOcat_all = fitsio.read(QSOcat_all_fp,'QSO_CAT')
-    #restrict to only dark time exposures
-    QSOcat_all = QSOcat_all[QSOcat_all['PROGRAM'] == 'dark']
-    
-    QSOcat_qso_fp = '/global/cfs/cdirs/desi/users/edmondc/QSO_catalog/{}/QSO_cat_{}_healpix_only_qso_targets.fits'.format(redux,redux)
-    QSOcat_qso = fitsio.read(QSOcat_qso_fp,'QSO_CAT')
-    QSOcat_qso = QSOcat_qso[QSOcat_qso['PROGRAM'] == 'dark']
-    
-    print('Initial all catalog length: {}'.format(len(QSOcat_all)))
-    print('Initial qso catalog length: {}'.format(len(QSOcat_qso)))
-    
-    #match_TID,all_idx,qso_idx = np.intersect1d(QSOcat_all['TARGETID'],QSOcat_qso['TARGETID'],return_indices=True)
-    all_both_mask = np.isin(QSOcat_all['TARGETID'],QSOcat_qso['TARGETID'])
-    
-    QSOcat = QSOcat_all[~all_both_mask]
-    
-    print('After removing qso target entries {} entries remain in catalog'.format(len(QSOcat)))
-    
-elif redux == 'loa':
-    reduction_base_dir = '/global/cfs/cdirs/desi/spectro/redux/{}/'.format(redux)
-    out_base_dir = '/pscratch/sd/l/lucasnap/MgII_Abs_Chains/{}'.format(redux)
-    
-    QSOcat_fp = '/global/cfs/cdirs/desi/survey/catalogs/DA2/QSO/loa/QSO_cat_loa_main_dark_healpix_only_qso_targets_v2.fits'
-    QSOcat = fitsio.read(QSOcat_fp,'QSO_CAT')
-    #restrict to only dark time exposures
-    QSOcat = QSOcat[QSOcat['PROGRAM'] == 'dark']
-    
-    print('{} entries in input QSO Catalog'.format(QSOcat.size))
-
-#create list of unique healpix values
-hp_vals = radec2pix(64,QSOcat['TARGET_RA'],QSOcat['TARGET_DEC'])
-hp_unique = np.unique(hp_vals)
-
-#routine to check if hp values have been completed
-#make a mask of healpix directories that have/have not been succesfully complete    
-hp_complete_mask = []
-
-for hp_val in hp_unique:
-    #recast as str
-    hp_str = str(hp_val)
-    
-    if int(hp_val) < 100:
-        hp_short = 0
-    else:
-        hp_short = hp_str[0:-2]
-
-    #format filepath to MgII output
-    out_dir = '{}/{}/{}'.format(out_base_dir,hp_short,hp_str)
-    h5_fn = 'MgII-Abs-Chains-{}.hdf5'.format(hp_str)
-    h5_fp = os.path.join(out_dir,h5_fn)
-
-    #see if a valid h5 file is written into path (TODO: a way to do this without try/except?)
-    try:
-        f = h5py.File(h5_fp,'r')
-        #if it is (i.e. no error thrown) we have completed this hp_val (mask[val]=True)
-        hp_complete_mask.append(True)
-    #if a valid h5 file hasn't been written. we can check if it exists and remove it, then proceed. Either way (mask[val]=False)
-    except OSError as e:
-        if(str(e)[0:19]=='Unable to open file' and os.path.exists(h5_fp)):
-            os.remove(h5_fp)
-        hp_complete_mask.append(False)
-
-#np-ify (TODO: better way to do this?)
-hp_complete_mask = np.asarray(hp_complete_mask)
-
-#find subset of incomplete hp_dirs
-hp_incomplete = hp_unique[~hp_complete_mask]
-print('{} total healpixels in release'.format(len(hp_unique)))
-print('{} incomplete healpixels in release'.format(len(hp_incomplete)))
-
-#take percentage of hp_incomplete corresponding to passed argv
-#currently defeaults to 10 percent interval
-if(len(sys.argv)==4):
-    hp_incomplete = hp_incomplete[int(len(hp_incomplete)*int(sys.argv[3])/100):int(len(hp_incomplete)*(int(sys.argv[3])/100+0.25))]
-    print('{} incomplete healpixels in release after applying percentage cut'.format(len(hp_incomplete)))
-
-print('Running MgII Absorption Finder on healpix directories: {}.'.format(hp_incomplete))
-
-#If there are no incomplete directories, survey is complete. Exit
-if(len(hp_incomplete)==0):
-    print('All healpix directories are already completed for release: {}'.format(redux))
-    sys.exit()
-
 #Function pool will pass each hp_unique entry to
 def RunFinder_HPind(hp_val):
-            
     #pull subset of QSO_cat in healpix area
     cat_sub = QSOcat[hp_vals==hp_val]
     #run feature detection and MCMC step
     Doublet_Detection(cat_sub,hp_val)
     print('Done: ',hp_val)
 
+def parse():
+    parser = ArgumentParser(description="This script produces hdf5 files with MgII absorber information for all spectra in a specific spectroscopic release.")
+    parser.add_argument("--redux", help = "Spectroscopic reduction to search for MgII Absorbers",type=str,default='fuji')
+    parser.add_argument("--redux_split", help = "Used to divide the reduction into multiple portions to be run across multiple nodes", type=int, default=1)
+    parser.add_argument("--split_num", help = "When redux_split > 1 this should be specified to determine which portion of the split to run", type=int, default=None)
+    parser.add_argument("--num_proc", help = "Number of processes to use, recommend 1 to 32 to avoid memory issues. When num_proc = 1 only a single healpixel will be run",type=int,default=1)
+    parser.add_argument("--outdir",help = "Directory to write results of MgII search. Subdirectories MgII_Abs_Chains/{redux} will be created.",type=str, default=os.environ['PSCRATCH'])
     
-if (str(sys.argv[2])=='safe'):
-    #run a single random entry with a single process
-    nprocesses = 1
-    print('{} processes being utilized'.format(nprocesses))
-    #RunFinder_HPind(np.random.choice(hp_incomplete))
-    
-    RunFinder_HPind(hp_incomplete[0])
+    args = parser.parse_args()
+    return args
 
-if (str(sys.argv[2])=='full'):
-    #Setup and run pool
-    pool = multiprocessing.Pool(processes = nprocesses)
-    print('{} processes being utilized'.format(nprocesses))
-    #run pool
-    pool.map(RunFinder_HPind, hp_incomplete)
+def main():
+    args = parse()
+
+    #These pased as command line args
+    redux = args.redux
+    global reduction_base_dir; global out_base_dir
+    reduction_base_dir = '/global/cfs/cdirs/desi/spectro/redux/{}/'.format(redux)
+    out_base_dir = os.path.join(args.outdir,'MgII_Abs_Chains/{}'.format(redux))
+    
+    #determine catalog path from redux. Requires adding a new entry for each spectroscopic release
+    if redux == 'fuji' or redux == 'guadalupe':  
+        QSOcat_fp = '/global/cfs/cdirs/desi/users/edmondc/QSO_catalog/{}/QSO_cat_{}_healpix_only_qso_targets.fits'.format(redux,redux)        
+    elif redux == 'iron':
+        QSOcat_fp = '/global/cfs/cdirs/desi/survey/catalogs/Y1/QSO/iron/QSO_cat_iron_main_dark_healpix_only_qso_targets_v0.fits'
+    elif redux == 'loa':
+        QSOcat_fp = '/global/cfs/cdirs/desi/survey/catalogs/DA2/QSO/loa/QSO_cat_loa_main_dark_healpix_only_qso_targets_v2.fits'
+    else:
+        print('Specified reduction:{} has no provided QSO catalog filepath'.format(redux))
+        sys.exit()
+
+    global QSOcat
+    QSOcat = fitsio.read(QSOcat_fp,'QSO_CAT')
+    #restrict to only dark time exposures
+    QSOcat = QSOcat[QSOcat['PROGRAM'] == 'dark']
+        
+    print('{} entries in input QSO Catalog'.format(QSOcat.size))
+    
+    #create list of unique healpix values
+    global hp_vals
+    hp_vals = radec2pix(64,QSOcat['TARGET_RA'],QSOcat['TARGET_DEC'])
+    hp_unique = np.unique(hp_vals)
+    
+    #routine to check if hp values have been completed
+    #make a mask of healpix directories that have/have not been succesfully complete    
+    hp_complete_mask = []
+    
+    for hp_val in hp_unique:
+        #recast as str
+        hp_str = str(hp_val)
+        
+        if int(hp_val) < 100:
+            hp_short = 0
+        else:
+            hp_short = hp_str[0:-2]
+    
+        #format filepath to MgII output
+        out_dir = '{}/{}/{}'.format(out_base_dir,hp_short,hp_str)
+        h5_fn = 'MgII-Abs-Chains-{}.hdf5'.format(hp_str)
+        h5_fp = os.path.join(out_dir,h5_fn)
+    
+        #see if a valid h5 file is written into path (TODO: a way to do this without try/except?)
+        try:
+            f = h5py.File(h5_fp,'r')
+            #if it is (i.e. no error thrown) we have completed this hp_val (mask[val]=True)
+            hp_complete_mask.append(True)
+        #if a valid h5 file hasn't been written. we can check if it exists and remove it, then proceed. Either way (mask[val]=False)
+        except OSError as e:
+            if(str(e)[0:19] == 'Unable to open file' and os.path.exists(h5_fp)):
+                os.remove(h5_fp)
+            hp_complete_mask.append(False)
+    
+    #np-ify (TODO: better way to do this?)
+    hp_complete_mask = np.asarray(hp_complete_mask)
+    
+    #find subset of incomplete hp_dirs
+    hp_incomplete = hp_unique[~hp_complete_mask]
+    print('{} total healpixels in release'.format(len(hp_unique)))
+    print('{} incomplete healpixels in release'.format(len(hp_incomplete)))
+    
+    #check for args redux split and divide by split number if necessary
+    if args.redux_split > 1:
+        start_pct = int(100*(args.split_num-1)/args.redux_split)
+        end_pct = int(100*args.split_num/args.redux_split)
+
+        hp_incomplete = hp_incomplete[int(hp_incomplete.size*start_pct/100):int(hp_incomplete.size*end_pct/100)]
+        print('Split {} contains {} healpixels to run'.format(args.split_num,hp_incomplete.size))
+    
+    print('Running MgII Absorption Finder on healpix directories: {}.'.format(hp_incomplete))
+    
+    #If there are no incomplete directories, survey is complete. Exit
+    if(len(hp_incomplete) == 0):
+        print('All healpix directories are already completed for release: {}'.format(redux))
+        sys.exit()
+          
+    if args.num_proc == 1:
+        #run a single random entry with a single process
+        nprocesses = 1
+        print('Running in single process safe mode')
+        RunFinder_HPind(hp_incomplete[0])
+    
+    else:
+        #Setup and run pool
+        pool = multiprocessing.Pool(processes = args.num_proc)
+        print('{} processes being utilized'.format(args.num_proc))
+        #run pool
+        pool.map(RunFinder_HPind, hp_incomplete)
+
+main()
